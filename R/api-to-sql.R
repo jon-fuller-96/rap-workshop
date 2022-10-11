7fedb618d300eebdce39495dc6a614cb99a2dc8b
Jonathan Fuller <jonathan.fuller@ukhsa.gov.uk>
2022-10-11 11:16:22 +0100

    rename file

--- R/api-to-sql.R
+++ R/api-to-sql.R
@@ -4,32 +4,30 @@
 #install.packages('httr')
 #install.packages('readr')
 #install.packages("scales")
+#install.packages("dplyr")
 
 
 library(DBI)
 library(logr)
 library(httr)
 library(scales)
+library(dplyr)
 
 # set API url
 url <- "https://api.coronavirus.data.gov.uk/v2/data?areaType=msoa&metric=cumPeopleVaccinatedFirstDoseByVaccinationDate&format=csv"
 # pull from API
 res <- httr::GET(url)
 # convert to df
-df_api <-  suppressMessages(httr::content(res))
+df_api <-  suppressMessages(httr::content(res)) %>% mutate(date = as.character(date))
+
+
 # log API data
-cat(scales::comma(nrow(df_api)), " - Rows pulled from API\n")
+cat(scales::comma(nrow(df_api)),
+    " - Rows pulled from API\n")
 
 # Create an ephemeral in-memory RSQLite database
 con <- dbConnect(RSQLite::SQLite(), ":memory:")
 
 # write to local SQL server
-dbWriteTable(con, "df_vax", df_api)
+dbWriteTable(con, "vax_tbl", df_api)
 
-# read from local server
-res <- dbSendQuery(con, "SELECT * FROM df_vax")
-df_vax <- dbFetch(res)
-cat(scales::comma(nrow(df_vax)), " - Rows pulled from SQL\n")
-
-# remove unneeded objects
-rm(list = c("con", "res", "df_api", "url"))
